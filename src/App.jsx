078e0ee50187a24905c3ece734b61f11e4997015
Andre Lima <andresdslima@gmail.com>
2022-06-20 20:07:17 +0200

    estrutura basica do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import dogIcon from "./assets/images/dogIcon.svg";
+import "./App.scss";
+
+function App() {
+	return (
+		<>
+			<h1>PetMatch</h1>
+			<img src={dogIcon} alt="Dog icon" />
+		</>
+	);
+}
+
+export default App;
+// pq qnd dou npm start, vai p localhost:3000/petmatch???
