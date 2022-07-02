3a3d591ecc7b370e31762c0dc596344cbbe0f1b2
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-02 19:48:15 -0300

    Adicionando rotas para paginas FormularioEnviado e FormularioAprovadoFInal

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,9 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import PetProfile from './pages/PetProfile'
+import FormularioEnviado from './pages/FormularioEnviado'
+import FormularioAprovadoFinal from './pages/FormularioAprovadoFinal'
+
 
 export default function AppRoutes() {
 	return (
@@ -13,7 +16,8 @@ export default function AppRoutes() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/petprofile" element={<PetProfile />} />
-
+				<Route path="/formularioenviado" element={<FormularioEnviado />} />
+				<Route path="/formularioaprovadofinal" element={<FormularioAprovadoFinal />} />
 			</Routes>
 		</Router>
 	);
