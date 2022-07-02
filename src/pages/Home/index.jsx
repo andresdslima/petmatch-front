9b64dd567293041bbc4e80a767a0760dd7d253ce
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-02 19:44:56 -0300

    Atualização componentes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,12 +3,14 @@ import Container from '../../components/Container';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import ImagesCarousel from '../../components/ImagesCarousel';
+import StepByStepAdoption from '../../components/StepByStepAdoption';
 
 const Home = () => {
     return (
         <>
            <Header />
             <ImagesCarousel />
+            <StepByStepAdoption />
             <Footer />
         </>
     );
