de733a651c82bdb66c92d8dac22ff60399edda13
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-03 22:26:40 -0300

    Adicionando pagina Matches

---
+++ src/pages/Matches/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import MatchesForm from '../../components/MatchesForm';
+
+const Matches = () => {
+    return (
+        <>
+        <Header />
+        <MatchesForm />
+        <Footer />
+        </>
+    )   
+}
+
+export default Matches;
