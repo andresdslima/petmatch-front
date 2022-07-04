f12ed108548a609282a14d8c22231b4e8f462905
Andre Lima <andresdslima@gmail.com>
2022-07-04 22:26:09 +0200

    ajustes nos layouts das paginas

--- src/pages/Adoption/index.jsx
+++ src/pages/Adoption/index.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import AdoptionForm from '../../components/AdoptionForm';
+import MatchsMainContent from '../../components/MatchsMainContent';
+import PeopleReview from '../../components/Section/PeopleReview';
+import CardPostsContainer from '../../components/Card-Posts-Container';
 
 const Matches = () => {
 	return (
 		<>
 			<Header />
+			
 			<AdoptionForm />
+			<MatchsMainContent />
+			<PeopleReview />
+			<CardPostsContainer />
 			<Footer />
 		</>
 	);
--- src/pages/Matches/index.jsx
+++ src/pages/Matches/index.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import MatchesForm from '../../components/MatchesForm';
+import PeopleReview from '../../components/Section/PeopleReview';
+import CardPostsContainer from '../../components/Card-Posts-Container';
 
 const Matches = () => {
-    return (
-        <>
-        <Header />
-        <MatchesForm />
-        <Footer />
-        </>
-    )   
-}
+	return (
+		<>
+			<Header />
+			
+			<MatchesForm />
+			<PeopleReview />
+			<CardPostsContainer />
+			<Footer />
+		</>
+	);
+};
 
 export default Matches;
