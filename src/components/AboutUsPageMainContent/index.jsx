9e385b6b66e4cbf1c6b1b5eb1da763c0235086a4
Andre Lima <andresdslima@gmail.com>
2022-07-06 01:27:41 +0200

    ajustes na pag sobre

--- src/components/AboutUsPageMainContent/index.jsx
+++ src/components/AboutUsPageMainContent/index.jsx
@@ -1,63 +1,133 @@
-import AboutUsCards from "../Cards/Card-AboutUsPage"
-import Image1 from "../../assets/images/fredericIcon.png"
-import Image2 from "../../assets/images/ramonIcon.png"
-import Image3 from "../../assets/images/andreIcon.png"
-import Image4 from "../../assets/images/marceloIcon.png"
-import Image5 from '../../assets/images/guilhermeIcon.png'
-import Image6 from '../../assets/images/eduardoIcon.png'
-import Image7 from '../../assets/images/felipeIcon.png'
-import Image8 from '../../assets/images/giovannaIcon.png'
-import Image9 from '../../assets/images/viniciusIcon.png'
-import Image10 from '../../assets/images/thaisIcon.png'
-import Image11 from '../../assets/images/victorIcon.png'
-import Image12 from '../../assets/images/joaoIcon.png'
+import AboutUsCards from '../Cards/Card-AboutUsPage';
+import Image1 from '../../assets/images/fredericIcon.png';
+import Image2 from '../../assets/images/ramonIcon.png';
+import Image3 from '../../assets/images/andreIcon.png';
+import Image4 from '../../assets/images/marceloIcon.png';
+import Image5 from '../../assets/images/guilhermeIcon.png';
+import Image6 from '../../assets/images/eduardoIcon.png';
+import Image7 from '../../assets/images/felipeIcon.png';
+import Image8 from '../../assets/images/giovannaIcon.png';
+import Image9 from '../../assets/images/viniciusIcon.png';
+import Image10 from '../../assets/images/thaisIcon.png';
+import Image11 from '../../assets/images/victorIcon.png';
+import Image12 from '../../assets/images/joaoIcon.png';
 
-import { Row, Col } from 'react-bootstrap'
-import './styles.css'
+import { Row, Col } from 'react-bootstrap';
+import './styles.css';
 
 const AboutUsPageMainContent = () => {
-    return (
-        <Row xs={1} sm={2} md={3} lg={4} id='grid' className="container">
-            <Col>
-                <AboutUsCards imageName={Image1} />
-            </Col>
+	return (
+		<Row xs={1} sm={2} md={3} lg={4} id="grid" className="container">
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/fredericlsr"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image1} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/ramon-qs"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image2} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/andrelimadev"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image3} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/marcelo-tesla"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image4} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/guilhermef-melo/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image5} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/carreiroe"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image6} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/felipe-matheus-26b147163"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image7} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/gioschneider"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image8} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/viniciusantonello"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image9} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/thaispcavalcante"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image10} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/victor-landim-37b102169/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image11} />
+				</a>
+			</Col>
+			<Col>
+				<a
+					href="https://www.linkedin.com/in/oliveirajg"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<AboutUsCards imageName={Image12} />
+				</a>
+			</Col>
+		</Row>
+	);
+};
 
-            <Col>
-                <AboutUsCards imageName={Image2} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image3} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image4} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image5} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image6} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image7} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image8} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image9} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image10} />
-            </Col>
-            <Col>
-                <AboutUsCards imageName={Image11} />
-            </Col>
-
-            <Col>
-                <AboutUsCards imageName={Image12} />
-            </Col>
-        </Row >
-    )
-}
-
-export default AboutUsPageMainContent
+export default AboutUsPageMainContent;
