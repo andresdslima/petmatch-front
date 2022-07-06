1de9749ad8923e9f7348f476385af4f2ec39efab
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-06 20:52:00 -0300

    Criando página ONGs

---
+++ src/pages/ONGs/index.jsx
@@ -0,0 +1,22 @@
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+import Banner from "../../components/Banner"
+import PeopleReview from "../../components/Section/PeopleReview"
+import CardPostsContainer from "../../components/Card-Posts-Container"
+import Partners from "../../components/Partners"
+
+
+const ONGs = () => {
+    return (
+        <>
+        <Header />
+        <Banner />
+        <Partners />
+        <PeopleReview />
+        <CardPostsContainer />
+        <Footer />
+        </>
+    )
+}
+
+export default ONGs;
