5b6b133b430bfde25131b1f8310006b5b641f509
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-07 00:20:22 -0300

    Componente Partner finalizado

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -10,33 +10,33 @@ import Puppies from '../../assets/images/Puppies.svg'
 
 const Partners = () => {
     return (
-        <Container>
-            <span>Conheça alguns membros da nossa família</span>
-            <S.PartnerContainer className='d-flex justify-content-start align-items-center'>
+        <Container className='my-5'>
+            <S.Title className='mb-5'>Conheça alguns membros da nossa família</S.Title>
+            <S.PartnerContainerLeft>
                 <S.ImageDiv>
-                    <S.Image src={CaoSemDono} alt="Cão sem Dono Logo" />
+                    <S.CaoSemDonoImage src={CaoSemDono} alt="Cão sem Dono Logo" />
                 </S.ImageDiv>
-                <S.PartnerDescription className='text-start'>O Cão Sem Dono é uma ONG (Organização Não Governamental), sem fins lucrativos, e que nasceu de um grande sonho do seu atual presidente: tirar o maior número possível de animais das ruas, dar tratamento adequado e integrá-los a famílias que lhes deem amor, carinho e uma vida digna.</S.PartnerDescription>
-            </S.PartnerContainer>
-            <S.PartnerContainer className='d-flex justify-content-end'>
-                <S.PartnerDescription className='text-end'>Com mais de 70 anos de existência, a SUIPA permanece viva, como uma entidade particular, não eutanásica, sem fins lucrativos, e de utilidade pública. Além do abrigo, a SUIPA mantém em sua sede uma Assistência Veterinária, com preços populares, para que todas as pessoas possam cuidar de seus animais de estimação.</S.PartnerDescription>
+                <S.PartnerDescription>O Cão Sem Dono é uma ONG (Organização Não Governamental), sem fins lucrativos, e que <br/>nasceu de um grande sonho do seu atual presidente: tirar o maior número possível de animais <br/>das ruas, dar tratamento adequado e integrá-los a famílias que lhes deem amor, carinho e uma <br/>vida digna.</S.PartnerDescription>
+            </S.PartnerContainerLeft>
+            <S.PartnerContainerRight>
+                <S.PartnerDescription>Com mais de 70 anos de existência, a SUIPA permanece viva, como uma entidade particular, <br/>não eutanásica, sem fins lucrativos, e de utilidade pública. Além do abrigo, a SUIPA mantém <br/>em sua sede uma Assistência Veterinária, com preços populares, para que todas as pessoas <br/>possam cuidar de seus animais de estimação.</S.PartnerDescription>
                 <S.ImageDiv>
-                    <S.Image src={Suipa} alt="Suipa Logo" />
+                    <S.SuipaImage src={Suipa} alt="Suipa Logo" />
                 </S.ImageDiv>
-            </S.PartnerContainer>
-            <S.PartnerContainer className='d-flex justify-content-start'>
+            </S.PartnerContainerRight>
+            <S.PartnerContainerLeft>
                 <S.ImageDiv>
-                    <S.Image src={AmparaAnimal} alt="Ampara Animal Logo" />
+                    <S.AmparaAnimalImage src={AmparaAnimal} alt="Ampara Animal Logo" />
                 </S.ImageDiv>
-                <S.PartnerDescription className='text-start'>A AMPARA nasce em agosto de 2010, pela coragem de Juliana Camargo e Marcele Becker em tirar diversos animais da situação de rua. Em 5 anos de trabalho pesado, a AMPARA consegue se tornar a maior OSCIP de proteção animal do Brasil, com o maior número de animais amparados.</S.PartnerDescription>
-            </S.PartnerContainer>
-            <S.PartnerContainer className='d-flex justify-content-end'>
-                <S.PartnerDescription className='text-end'>Fundado em fevereiro de 2015, o Instituto Luisa Mell atua principalmente no resgate de animais feridos ou em situação de risco, recuperação e adoção. Mantemos um abrigo com cerca de 300 animais, entre cães e gatos, todos resgatados das ruas, onde eles são protegidos, alimentados e aguardam pela chance de serem adotados.</S.PartnerDescription>
+                <S.PartnerDescription>A AMPARA nasce em agosto de 2010, pela coragem de Juliana Camargo e Marcele Becker em <br/>tirar diversos animais da situação de rua. Em 5 anos de trabalho pesado, a AMPARA consegue <br/>se tornar a maior OSCIP de proteção animal do Brasil, com o maior número de animais <br/>amparados.</S.PartnerDescription>
+            </S.PartnerContainerLeft>
+            <S.PartnerContainerRight>
+                <S.PartnerDescription>Fundado em fevereiro de 2015, o Instituto Luisa Mell atua principalmente no resgate de <br/>animais feridos ou em situação de risco, recuperação e adoção. Mantemos um abrigo com <br/>cerca de 300 animais, entre cães e gatos, todos resgatados das ruas, onde eles são <br/>protegidos, alimentados e aguardam pela chance de serem adotados.</S.PartnerDescription>
                 <S.ImageDiv>
-                    <S.Image src={InstitutoLuisaMel} alt="Cão sem Dono Logo" />
+                    <S.InstitutoLuisaMelImage src={InstitutoLuisaMel} alt="Instituto Luisa Mel Logo" />
                 </S.ImageDiv>
-            </S.PartnerContainer>
-            <img src={Puppies} alt="" />
+            </S.PartnerContainerRight>
+            <S.PuppiesImage src={Puppies} alt="" />
         </Container>
     )
 }
--- src/components/Partners/styled.js
+++ src/components/Partners/styled.js
@@ -1,18 +1,132 @@
 import styled from "styled-components";
 
-export const PartnerContainer = styled.div`
-gap: 20px;
+export const Title = styled.div`
+font-size: 1.56vw;
+font-weight: 600;
+
+@media only screen and (max-width: 500px) {
+  font-size: 12px;
+  }
+`
+
+export const PartnerContainerLeft = styled.div`
+display: flex;
+justify-content: flex-start;
+gap: 4.55vw;
+align-items: center;
+padding: 50px 0;
+text-align: start;
+position: relative;
+
+&::before {
+    width: 24.6vw;
+    border-bottom: 1px solid #9FA1A6;
+    content: '';
+    position: absolute;
+    left:37.5%;
+    bottom:0;
+  }
+
+@media only screen and (max-width: 500px) {
+  flex-direction: column;
+  text-align: center;
+  padding: 25px 0;
+  }
+`
+
+export const PartnerContainerRight = styled(PartnerContainerLeft)`
+justify-content: flex-end;
+text-align: end;
+
+@media only screen and (max-width: 500px) {
+  flex-direction: column-reverse;
+  text-align: center;
+  }
 `
 
 export const ImageDiv = styled.div`
-width: 10vw;
+display: flex;
+justify-content: center;
+width: 12.8vw;
+
+@media only screen and (max-width: 900px) {
+  width: 19.2vw;
+  }
+
+@media only screen and (max-width: 500px) {
+  width: 25.6vw;
+  }
 `
 
-export const Image = styled.img`
-width: 10vw;
+export const CaoSemDonoImage = styled.img`
+width: 12.8vw;
+
+@media only screen and (max-width: 900px) {
+  width: 19.2vw;
+  }
+
+@media only screen and (max-width: 500px) {
+  width: 25.6vw;
+  }
+`
+
+export const SuipaImage = styled.img`
+width: 12.65vw;
+
+@media only screen and (max-width: 900px) {
+  width: 19.98vw;
+  }
+
+@media only screen and (max-width: 500px) {
+  width: 25.3vw;
+  }
+`
+
+export const AmparaAnimalImage = styled.img`
+width: 12.5vw;
+
+@media only screen and (max-width: 900px) {
+  width: 18.75vw;
+  }
+
+@media only screen and (max-width: 500px) {
+  width: 25vw;
+  }
+`
+
+export const InstitutoLuisaMelImage = styled.img`
+width: 6.88vw;
+
+@media only screen and (max-width: 900px) {
+  width: 10.32vw;
+  }
+
+@media only screen and (max-width: 500px) {
+  width: 13.76vw;
+  }
 `
 
 export const PartnerDescription = styled.p`
-width: 73%;
-font-size: 17px;
+font-size: 1.1vw;
+margin: 0;
+
+@media only screen and (max-width: 900px) {
+    font-size: 10px;
+  }
+
+@media only screen and (max-width: 500px) {
+    font-size: 7px;
+  }
+`
+
+export const PuppiesImage = styled.img`
+width: 26.25vw;
+
+@media only screen and (max-width: 900px) {
+  width: 39.37vw;
+  }
+
+@media only screen and (max-width: 500px) {
+  width: 52.5vw;
+  }
 `
