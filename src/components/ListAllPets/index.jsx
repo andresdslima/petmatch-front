0ad1684386732fb16ff3ab73e79d5a1520903a1d
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-10 22:02:40 -0300

    Criando componente Com listagem de todos os pets

---
+++ src/components/ListAllPets/index.jsx
@@ -0,0 +1,48 @@
+import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPetList } from '../../store/modules/pets';
+import { getPets } from '../../services/mainAPI/pets';
+import { Link } from 'react-router-dom';
+import * as S from './styled';
+import { getUserById } from '../../services/mainAPI/users';
+
+const ListAllPets = () => {
+	const petList = useSelector(state => state.petsSlice.pets);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		getPets().then(pets => dispatch(setPetList(pets)));
+	}, [dispatch]);
+
+	return (
+		<>
+		<S.TitleBackground className="mt-5">
+				<S.Title>Nossos pets</S.Title>
+			</S.TitleBackground>
+		<S.PetsContainer>
+			<S.RowContainer xs={2} sm={2} md={3} lg={4} xl={5}>
+				{petList.map(pet => (
+					<S.Column key={pet.id}>
+						<Link exact to={`/petprofile/?${pet.id}`}>
+							<S.CardContainer>
+								<S.CardImage variant="top" src={pet.petImage} />
+								<S.CardBody>
+									<S.CardTitle>{pet.nome}</S.CardTitle>
+									<S.CardText className="cardText">
+										{/* {`${getPetLocation(pet.userID)}`} */}
+										{/* {pet.userID} - {teste2[index].userID} */}
+										{pet.userID} São Paulo - SP
+									</S.CardText>
+								</S.CardBody>
+							</S.CardContainer>
+						</Link>
+					</S.Column>
+				))}
+			</S.RowContainer>
+		</S.PetsContainer>
+		</>
+	);
+};
+
+export default ListAllPets;
