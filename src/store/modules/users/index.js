6f27b65991eefd6ec2dbf6bea0860bf88cb01058
Andre Lima <andresdslima@gmail.com>
2022-07-12 20:39:51 +0200

    usuario precisa completar cadastro para cadastrar pet

--- src/store/modules/users/index.js
+++ src/store/modules/users/index.js
@@ -1,12 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	isLogged: false,
-	accessToken: '',
-	firstName: '',
-	userStatus: 0,
-	permission: 0,
-	id: 0,
+	login: {
+		isLogged: false,
+		accessToken: '',
+		firstName: '',
+		userStatus: 0,
+		permission: 0,
+		id: 0,
+	},
+	user: {},
 };
 
 const usersSlice = createSlice({
@@ -15,12 +18,23 @@ const usersSlice = createSlice({
 	reducers: {
 		signIn(state, action) {
 			Object.assign(state, {
-				isLogged: true,
-				accessToken: action.payload.accessToken,
-				firstName: action.payload.firstName,
-				userStatus: action.payload.userStatus,
-				permission: action.payload.permission,
-				id: action.payload.id,
+				login: {
+					isLogged: true,
+					accessToken: action.payload.accessToken,
+					firstName: action.payload.firstName,
+					userStatus: action.payload.userStatus,
+					permission: action.payload.permission,
+					id: action.payload.id,
+				},
+			});
+		},
+		storeUser(state, action) {
+			Object.assign(state, {
+				// ...state,
+				user: {
+					cidade: action.payload.cidade,
+					id: action.payload.id,
+				},
 			});
 		},
 		signOut(state, action) {
@@ -29,6 +43,6 @@ const usersSlice = createSlice({
 	},
 });
 
-export const { signIn, signOut } = usersSlice.actions;
+export const { signIn, storeUser, signOut } = usersSlice.actions;
 
 export default usersSlice.reducer;
