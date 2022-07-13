24a747756f7dcd9bc592dac4b93ce10d55c1f979
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-07-13 00:06:49 -0300

    Form user

--- src/components/UserEditForm/index.jsx
+++ src/components/UserEditForm/index.jsx
@@ -11,72 +11,45 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const UserEditForm = () => {
-	const userItem = useSelector(state => state.persistedReducer.user);
-	const data = JSON.parse(localStorage.getItem('data'));
-	const accessToken = data.payload.accessToken;
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+	// const userItem = useSelector(state => state.persistedReducer.user);
+	// const data = JSON.parse(localStorage.getItem('data'));
+	// const accessToken = data.payload.accessToken;
+	// const dispatch = useDispatch();
+	// const navigate = useNavigate();
 
-	function handleChange(event) {
-		const file = event.target.files[0];
-		formik.setFieldValue('petImage', file);
-	}
+	// function handleChange(event) {
+	// 	const file = event.target.files[0];
+	// 	formik.setFieldValue('petImage', file);
+	// }
 
-	useEffect(() => {
-		getUserById(data.payload.id).then(user => dispatch(storeUser(user)));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	// useEffect(() => {
+	// 	getUserById(data.payload.id).then(user => dispatch(storeUser(user)));
+	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
+	// }, []);
 
 	const formik = useFormik({
 		initialValues: {
-			especie: '',
-			idade: 0,
-			sexo: '',
-			peso: 0,
-			porte: '',
-			tamanho: 0,
-			cor: '',
-			raca: '',
-			castrado: false,
-			vacinado: false,
-			chip: '',
-			sobre: '',
-			petImage: undefined,
-			nome: '',
+			cpf: null,
+			contato: null,
+			cep: null,
+			logradouro: null,
+			numero_logradouro: null,
+			complemento: null,
+			bairro: null,
+			cidade: null,
+			uf: null,
+			sobre: null,
 		},
 
 		// validationSchema,
 
 		onSubmit: async values => {
-			api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-			api.defaults.headers.common['Content-Type'] = 'multipart/form-data';
+			// api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
-			if (!userItem.cidade) {
-				alert('É necessário completar seu cadastro primeiro');
-				navigate(`/users/${userItem.id}`);
-				return;
-			}
+			// const data = await postPets(formData);
 
-			const formData = new FormData();
-			formData.append('especie', values.especie);
-			formData.append('nome', values.nome);
-			formData.append('idade', values.idade);
-			formData.append('sexo', values.sexo);
-			formData.append('peso', values.peso);
-			formData.append('tamanho', values.tamanho);
-			formData.append('porte', values.porte);
-			formData.append('cor', values.cor);
-			formData.append('raca', values.raca);
-			formData.append('castrado', values.castrado);
-			formData.append('vacinado', values.vacinado);
-			formData.append('chip', values.chip);
-			formData.append('sobre', values.sobre);
-			formData.append('petImage', values.petImage, 'petImage.png');
-
-			const data = await postPets(formData);
-
-			dispatch(addNewPet({ pets: data }));
-			formik.resetForm();
+			// dispatch(addNewPet({ pets: data }));
+			// formik.resetForm();
 		},
 	});
 
@@ -110,6 +83,16 @@ const UserEditForm = () => {
 							/>
 						</Form.Group>
 						<Form.Group>
+							<S.Label>CEP</S.Label>
+							<S.FormItem
+								id="cep"
+								type="number"
+								value={formik.values.cep}
+								required
+								onChange={formik.handleChange}
+							/>
+						</Form.Group>
+						<Form.Group>
 							<S.Label>Logradouro</S.Label>
 							<S.FormItem
 								id="logradouro"
@@ -129,7 +112,7 @@ const UserEditForm = () => {
 								onChange={formik.handleChange}
 							/>
 						</Form.Group>
-                        <Form.Group>
+						<Form.Group>
 							<S.Label>Complemento</S.Label>
 							<S.FormItem
 								id="complemento"
@@ -150,7 +133,7 @@ const UserEditForm = () => {
 								onChange={formik.handleChange}
 							/>
 						</Form.Group>
-                        <Form.Group>
+						<Form.Group>
 							<S.Label>Cidade</S.Label>
 							<S.FormItem
 								id="cidade"
@@ -160,48 +143,47 @@ const UserEditForm = () => {
 								required
 								onChange={formik.handleChange}
 							/>
-                            <Form.Group>
-							<S.Label>Uf</S.Label>
-							<S.FormItemSelect
-								id="uf"
-                                name="uf"
-								type="select"
-								value={formik.values.uf}
-								required
-								onChange={formik.handleChange}
-							>
-								<S.SelectOption value=""></S.SelectOption>
-								<S.SelectOption value="AC">
-									AC
-								</S.SelectOption>
-								<S.SelectOption value="Pitbull">Pitbull</S.SelectOption>
-								<S.SelectOption value="Shitzu">Shit-zu</S.SelectOption>
-								<S.SelectOption value="Pug">Pug</S.SelectOption>
-								<S.SelectOption value="Maltês">Maltês</S.SelectOption>
-								<S.SelectOption value="Buldogue">Buldogue</S.SelectOption>
-								<S.SelectOption value="Spitz Alemão">
-									Spitz Alemão
-								</S.SelectOption>
-								<S.SelectOption value="Dachshund">Dachshund</S.SelectOption>
-								<S.SelectOption value="Pastor-Alemão">
-									Pastor-Alemão
-								</S.SelectOption>
-								<S.SelectOption value="Basset">Basset</S.SelectOption>
-								<S.SelectOption value="Schnauzer">Schnauzer</S.SelectOption>
-								<S.SelectOption value="Poodle">Poodle</S.SelectOption>
-								<S.SelectOption value="Persa">Persa</S.SelectOption>
-								<S.SelectOption value="Himalaia">Himalaia</S.SelectOption>
-								<S.SelectOption value="Siamês">Siamês</S.SelectOption>
-								<S.SelectOption value="Maine Coon">Maine Coon</S.SelectOption>
-								<S.SelectOption value="Angorá">Angorá</S.SelectOption>
-								<S.SelectOption value="Sphynx">Sphynx</S.SelectOption>
-								<S.SelectOption value="Ragdoll">Ragdoll</S.SelectOption>
-								<S.SelectOption value="Ashera">Ashera</S.SelectOption>
-								<S.SelectOption value="American Shorthair">
-									American Shorthair
-								</S.SelectOption>
-							</S.FormItemSelect>
-						</Form.Group>
+							<Form.Group>
+								<S.Label>Uf</S.Label>
+								<S.FormItemSelect
+									id="uf"
+									name="uf"
+									type="select"
+									value={formik.values.uf}
+									required
+									onChange={formik.handleChange}
+								>
+									<S.SelectOption value=""></S.SelectOption>
+									<S.SelectOption value="AC">AC</S.SelectOption>
+									<S.SelectOption value="AC">AC</S.SelectOption>
+									<S.SelectOption value="AL">AL</S.SelectOption>
+									<S.SelectOption value="AP">AP</S.SelectOption>
+									<S.SelectOption value="AM">AM</S.SelectOption>
+									<S.SelectOption value="BA">BA</S.SelectOption>
+									<S.SelectOption value="CE">CE</S.SelectOption>
+									<S.SelectOption value="DF">DF</S.SelectOption>
+									<S.SelectOption value="ES">ES</S.SelectOption>
+									<S.SelectOption value="GO">GO</S.SelectOption>
+									<S.SelectOption value="MA">MA</S.SelectOption>
+									<S.SelectOption value="MT">MT</S.SelectOption>
+									<S.SelectOption value="MS">MS</S.SelectOption>
+									<S.SelectOption value="MG">MG</S.SelectOption>
+									<S.SelectOption value="PA">PA</S.SelectOption>
+									<S.SelectOption value="PB">PB</S.SelectOption>
+									<S.SelectOption value="PR">PR</S.SelectOption>
+									<S.SelectOption value="PE">PE</S.SelectOption>
+									<S.SelectOption value="PI">PI</S.SelectOption>
+									<S.SelectOption value="RJ">RJ</S.SelectOption>
+									<S.SelectOption value="RN">RN</S.SelectOption>
+									<S.SelectOption value="RS">RS</S.SelectOption>
+									<S.SelectOption value="RO">RO</S.SelectOption>
+									<S.SelectOption value="RR">RR</S.SelectOption>
+									<S.SelectOption value="SC">SC</S.SelectOption>
+									<S.SelectOption value="SP">SP</S.SelectOption>
+									<S.SelectOption value="SE">SE</S.SelectOption>
+									<S.SelectOption value="TO">TO</S.SelectOption>
+								</S.FormItemSelect>
+							</Form.Group>
 						</Form.Group>
 						<S.TextareaContainer>
 							<S.Label>Sobre</S.Label>
